1b4012147849fa721e9857d36c1482c75c9b821b
mkinfrared <mkinfrared@gmail.com>
2018-09-29 12:04:45 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './_App.scss';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				HELLO WORLD
+			</div>
+		);
+	}
+}
+
+export default App;

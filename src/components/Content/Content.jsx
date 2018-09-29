c78d2b05bc5e3c66b4a2743a98f0c617a107ce19
mkinfrared <mkinfrared@gmail.com>
2018-09-29 13:14:53 +1000

    added MainView

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Sidebar from '../Sidebar/Sidebar';
+import MainView from '../MainView/MainView';
 
 function Content(props) {
 	return (
 		<div className='content'>
 			<Sidebar/>
+			<MainView/>
 		</div>
 	);
 }

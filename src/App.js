459eb4486a3763925b32d6d381516751b0085a28
mkinfrared <mkinfrared@gmail.com>
2018-09-29 12:19:38 +1000

    added Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-import './_App.scss';
+import Header from './components/Header/Header';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				HELLO WORLD
+				<Header/>
+				<hr/>
 			</div>
 		);
 	}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header(props) {
+	return (
+		<div className='header'>
+			<div></div>
+			<div></div>
+			<div>
+				<p>+7 (499) 346-21-31</p>
+				<a href="#">manager@pixelplus.ru</a>
+				<a href="#">www.pixelplus.ru</a>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
